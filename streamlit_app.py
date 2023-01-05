fd4675212e2d5d561dcbfb635e7eb093d946f847
Srihitha Kondaveeti <122000949+SRIHITHAKONDAVEETI@users.noreply.github.com>
2023-01-05 13:21:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-boiled Free_Range Egg')
-stremlit.text('🥑🍞Avacado toast'
+stremlit.text('🥑🍞Avacado toast')
